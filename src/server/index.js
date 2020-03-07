4340540deae2d3d820e0090386f6d6dbe3ced7af
Duc Le <leanhducvt@live.com>
2020-03-07 21:57:37 +0700

    feat: sdk setup for aylien

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,20 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+console.log(`Your API key is ${process.env.API_KEY}`)
+
 const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const aylien = require('aylien_textapi')
+
+// set aylien API credentias
+const textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
 
 const app = express()
 app.use(cors())
